eeda254519b14a9c27557d43ddbb6a9d09b53ab1
Cathrin Whitmer <catwhitmer@gmail.com>
2020-04-13 16:33:34 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,59 @@
 class TweetsController < ApplicationController
 
-
+  get '/tweets' do
+    if logged_in?
+      erb :tweets
+    else
+      redirect to "/users/login"
+    end
+  end
+  
+  get '/tweets/new' do 
+    erb :'/tweets/new'
+  end
+  
+  post '/tweets' do 
+    if logged_in?
+      @tweet = Tweet.create(content: params[:content], user_id: current_user.id)
+      redirect "/tweets/#{@tweet.id}"
+    else
+      redirect :'tweets/new'
+    end
+  end
+  
+  get '/tweets/:id' do 
+    @tweet = Tweet.find_by_id(params[:id])
+    erb :'/tweets/show_tweet'
+  end
+  
+  get '/tweets/:id/edit' do 
+    @tweet = Tweet.find_by_id(params[:id])
+      if logged_in?
+        erb :'/tweets/edit_tweet'
+      else 
+        redirect :'/users/login'
+      end
+  end
+  
+  patch '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+      if logged_in? && !params[:content].empty?
+        @tweet.update(content: params[:content])
+      
+        redirect "/tweets/#{@tweet.id}"
+      else 
+        redirect :'/'
+    end
+  end
+  
+  delete '/tweets/:id' do
+    if logged_in? && current_user
+    @tweet = Tweet.find_by_id(params[:id])
+      if @tweet.destroy
+        redirect '/tweets'
+      else 
+        redirect '/tweets'
+      end
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,42 @@
 class UsersController < ApplicationController
 
+  get '/create_user' do 
+    erb :'/users/create_user'
+  end
+  
+  get '/login' do
+    erb :'/users/login'
+  end
+  
+  post '/users/login' do 
+    @user = User.find_by(email: params[:email])
+    
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id 
+      redirect "/users/#{@user.id}"
+    else
+      redirect '/users/login'
+    end
+  end
+  
+  get '/users/:id' do 
+    @user = User.find_by_id(params[:id])
+    erb :"/users/show"
+  end
+  
+  post '/users' do 
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    if @user.save
+      session[:user_id] = @user.id
 
+      redirect to "/users/#{@user.id}"
+    else
+      redirect to '/create_user'
+    end
+  end
+  
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
 end
